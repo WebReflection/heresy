5c48f9250f52f2d946ac80dbbe02f8a76c75ecaa
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-07-25 11:16:18 -0500

    updated dependencies

--- index.js
+++ index.js
@@ -345,6 +345,11 @@ var heresy = (function (document,exports) {
 
           return had;
         },
+        forEach: function forEach(callback, context) {
+          k.forEach(function (key, i) {
+            callback.call(context, v[i], key, this);
+          }, this);
+        },
         get: function get(key) {
           return contains(key) ? v[i] : void 0;
         },
